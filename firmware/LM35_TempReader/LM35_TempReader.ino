4fde1b3a2a76b7b4318f27f2e2725c79dd8f1794
Nguyen Duc Luong <luongkhohn@gmail.com>
2026-04-15 22:10:05 +0700

    feat: doi dinh dang Serial sang JSON

--- firmware/LM35_TempReader/LM35_TempReader.ino
+++ firmware/LM35_TempReader/LM35_TempReader.ino
@@ -1,6 +1,6 @@
-int adcValues[2]; 
-float nhietDo[2]; 
-char chuoi[30];
+int adcValues[2];
+float nhietDo[2];
+char chuoi[50];
 
 void setup() {
   Serial.begin(9600);
@@ -13,8 +13,8 @@ void loop() {
   
   adcValues[1] = analogRead(A1);
   nhietDo[1] = (adcValues[1] * 500.0) / 1023.0;
-  
-  sprintf(chuoi, "%d,%d\n", (int)nhietDo[0], (int)nhietDo[1]);
+
+  sprintf(chuoi, "{\"A0\":%d, \"A1\":%d}\n", (int)nhietDo[0], (int)nhietDo[1]);
   Serial.print(chuoi);
   delay(100);
 }
